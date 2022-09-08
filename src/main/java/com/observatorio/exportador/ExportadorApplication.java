07ccf36a14b8d4b3adc8626de6ae993d50bd65d3
Henrique Paes de Souza <hpaesdesouza@gmail.com>
2022-09-08 11:52:03 -0300

    initial commit

---
+++ src/main/java/com/observatorio/exportador/ExportadorApplication.java
@@ -0,0 +1,13 @@
+package com.observatorio.exportador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExportadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExportadorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/observatorio/exportador/ExportadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.observatorio.exportador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExportadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
