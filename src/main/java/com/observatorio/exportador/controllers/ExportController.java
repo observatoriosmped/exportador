b8f3d07832dcc8788aeca3cf672e7e4147b81e7e
Izaque Sena <izaqueseninha@gmail.com>
2022-11-09 19:18:42 -0300

    tratamento de erros no exportador

--- src/main/java/com/observatorio/exportador/controllers/ExportController.java
+++ src/main/java/com/observatorio/exportador/controllers/ExportController.java
@@ -4,10 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,20 +18,21 @@ public class ExportController {
 	
 	@GetMapping("/por-nome")
 	public HttpEntity<byte[]> fileDownload(@RequestParam String fileName) throws IOException {
-		byte[] arquivo = Files.readAllBytes( Paths.get("/home/garsoft/Documentos/" + fileName) );
-//        HttpHeaders httpHeaders = new HttpHeaders();
-//        System.out.println(fileName);
-//        httpHeaders.add("Content-Disposition", "attachment;filename=" + fileName);
-//        HttpEntity<byte[]> entity = new HttpEntity<byte[]>( arquivo, httpHeaders)
-//        return entity;
-		
+		byte[] arquivo = null;
 	         
-	        String contentType = "text/csv";
-	        String headerValue = "attachment; filename=" + "\"" + fileName + "\"";
-	         
-	        return ResponseEntity.ok()
-	                .contentType(MediaType.parseMediaType(contentType))
-	                .header(HttpHeaders.CONTENT_DISPOSITION, headerValue)
-	                .body(arquivo);
+		String contentType = "text/csv";
+		String headerValue = "attachment; filename=" + "\"" + fileName + "\"";
+
+		try {
+			arquivo = Files.readAllBytes( Paths.get("/home/garsoft/Documentos/" + fileName) );
+			return ResponseEntity.ok()
+			.contentType(MediaType.parseMediaType(contentType))
+			.header(HttpHeaders.CONTENT_DISPOSITION, headerValue)
+			.body(arquivo);
+		}
+		catch (Exception e) {
+			System.out.println("can't export file");
+			return new ResponseEntity<byte[]>(arquivo, HttpStatus.BAD_GATEWAY);
+		}
     }
 }
